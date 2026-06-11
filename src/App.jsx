32a0e0eb8e172a3c54f87b8a335cf3c58a895629
dimastadeoo <dimastadeoo@gmail.com>
2026-06-11 11:15:22 +0700

    docs: create a vite init file in the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+function App() {
+  return (
+    <div className="flex justify-center item-center min-width-[100vh]">
+        <div className="flex gap-4">
+            hello world
+        </div>
+    </div>
+  )
+}
+
+export default App
